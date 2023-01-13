adc045dfc9e2ed987c2dec4dcf20f5448d086dc9
c0admin <mustafagldren@gmail.com>
2023-01-13 14:32:10 +0300

    Proje dosyası ekle.

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,29 @@
+﻿// klavyeden girilen herhangi bir karakterin ne oldugunu belirleme
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char x;
+    cout << "klavyeden herhangi bir tusa basiniz :";
+    cin >> x;
+
+
+    if (x>='0' && x<='9' ) {
+        cout << "girilen deger sayidir." << endl;
+
+    }
+    else if ((x >= 'a' && x <= 'z') || (x>='A' && x<='Z')) {
+        cout << "girilen deger harftir."<<endl;
+
+    }
+    else {
+        cout <<"girilen deger ozel isarettir."<<endl;
+    }
+
+    system("pause");
+    return 0;
+}
+
+
